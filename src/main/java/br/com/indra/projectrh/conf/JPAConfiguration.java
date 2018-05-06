5c4e5bcbf3a9eeecac02f6cc66c6c6a3fc22424e
sjsouza <faculdadesjs@gmail.com>
2018-05-06 02:34:10 -0300

    configuration correta

--- src/main/java/br/com/indra/projectrh/conf/JPAConfiguration.java
+++ src/main/java/br/com/indra/projectrh/conf/JPAConfiguration.java
@@ -1,49 +1,36 @@
 package br.com.indra.projectrh.conf;
 
-import java.util.Properties;
-
-import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
+@Configuration
 public class JPAConfiguration {
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/testeindra");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 
-        factoryBean.setJpaVendorAdapter(vendorAdapter);
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/testeindra");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	}
 
-        factoryBean.setDataSource(dataSource);
-
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        props.setProperty("hibernate.show_sql", "true");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-
-        factoryBean.setJpaProperties(props);
-
-        factoryBean.setPackagesToScan("br.com.jovens.profissionais.model");
-
-        return factoryBean;
-    }
-    
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-        return new JpaTransactionManager(emf);
-    }
 }
