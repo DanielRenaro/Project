5c0f5322ef36f6db550f8c71e5ad212cbec07bac
danielrenaro@gmail.com <drenaro@indra.es>
2018-05-06 20:25:08 -0300

    Simula banco

---
+++ src/main/java/com/indra/projectrh/model/SimulaBanco.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.indra.projectrh.model;
+
+import java.util.ArrayList;
+
+import com.indra.projectrh.model.JP;
+import com.indra.projectrh.model.Manager;
+import com.indra.projectrh.model.People;
+import com.indra.projectrh.model.User;
+
+/**
+ * @author Daniel Renaro
+ * @category Projeto RH DAO
+ * @date 06/05/2018
+ * @version 1.0
+ */
+public class SimulaBanco {
+
+	private ArrayList<User> users ;
+	private ArrayList<Manager> managers;
+	private ArrayList<People> peoples;
+	private ArrayList<JP> jps;
+	
+	
+	
+}
